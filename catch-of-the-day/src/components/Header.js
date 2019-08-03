8391306d2909fe80d8749221efaeb587cce93275
Romaine De La Cruz <r2dcruz@gmail.com>
2019-08-02 22:58:47 -0700

    Creating the first components

---
+++ catch-of-the-day/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Header extends React.Component {
+    render() {
+        return(
+            <header className="top">
+                <h1>Catch of the day</h1>
+                <h3 className="tagline">
+                </h3>
+            </header>
+            
+        )
+    }
+}
+
+export default Header;

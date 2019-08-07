f305329b9d72e0ca2fdca9261d768f4335e6502a
Romaine De La Cruz <r2dcruz@gmail.com>
2019-08-06 23:39:36 -0700

    passing props and worked on router

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,24 @@
 import React, {Fragment} from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+
+    myInput = React.createRef();
+
+    goToStore = event => {
+        // 1. stop the form from submitting
+        event.preventDefault();
+        // 2. get the text from that input
+        console.log(this.myInput    );
+
+        // 3. change the page to /store/what's-next 
+    }
+
     render() {
     return (
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
             <h2>Please enter store</h2>
-            <input type="text" required placeholder="Store Name"/>
+            <input type="text" ref={this.myInput} required placeholder="Store Name" defaultValue={getFunName()}/>
             <button type ="submit">Visit Store</button>
         </form>
         )

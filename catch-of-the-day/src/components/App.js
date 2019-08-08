99ff43d8c29c1ec518eabd69aa111491ebc990a5
Romaine De La Cruz <r2dcruz@gmail.com>
2019-08-07 23:17:45 -0700

    understanding state, might have a bug on calling value {.current}

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,20 +2,36 @@ import React from "react";
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import AddFishForm from "./AddFishForm";
+
 
 class App extends React.Component{
+    state = {
+        fishes : {},
+        Order: {}
+    };
+
+    addFish = fish => {
+        // 1. Take a copy of the existing state
+        const fishes = {...this.state.fishes};
+        // 2. Add our new fish to that fishes variable
+        fishes[`fish${Date.now()}`] = fish;
+        // 3. set the new fishes object to state
+        this.setState({fishes}); 
+    };
+
 render() {
     return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market"/>
                 </div>
-                <Inventory/>
                 <Order/>
+                <Inventory addFish={this.addFish}/>
             </div>
 
         )
         }
-    }
+    };
 
 export default App;
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,16 +2,15 @@ import React, {Fragment} from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
-
     myInput = React.createRef();
 
     goToStore = event => {
         // 1. stop the form from submitting
         event.preventDefault();
         // 2. get the text from that input
-        console.log(this.myInput    );
-
-        // 3. change the page to /store/what's-next 
+        const storeName = this.myInput.current.value;
+        // 3. change the page to /store/what's-next
+        this.props.history.push('/store/${storeName}') 
     }
 
     render() {
